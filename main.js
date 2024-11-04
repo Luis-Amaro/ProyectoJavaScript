8e877fc8eddbb27fd160a8c017ebf315c0b9a72a
Luis Amaro <elluchoamaro@gmail.com>
2024-11-04 01:34:21 -0300

    Proyecto avanzado

---
+++ main.js
@@ -0,0 +1,61 @@
+let accesoatienda = confirm("¿Quiere ingresar a LAZ, tienda de productos electrónicos?")
+
+
+const Productos= ["1-Smartphone, $40000","2-Laptop hp 15.6 pulgadas $50000", "3-Barra de sonido samsung $16000 ", "4-Sony PS5 slim $35000", "5-SmartTV Samsung Crystal 50 pulgadas $25000"]
+
+
+let listaproductos= Productos.join("\n") 
+
+const mostrarproductos= () => {
+    for (let i=0; i<Productos.length; i++){
+        alert (listaproductos)
+        break
+    }   
+}
+
+const carrito = []
+
+const agregaralcarrito =(productos) => {
+    carrito.push({
+        productos
+    })
+}
+
+
+const core = () => {
+    let opcion = Number(prompt("Bienvenido a LAZ, la mejor tienda onlione de productos electrónicos, a continuación indique la opción que corresponda: \n 1-Mostrar Productos Disponibles \n 2-Agregar Productos al Carrito"))
+
+    let bandera = true
+    let producto
+    while(bandera){
+        switch(opcion){
+            case 1:
+                mostrarproductos()
+                bandera = confirm ("¿Quiere realizar algun otro movimiento?")
+            break
+            case 2:
+                agregaralcarrito()
+                producto = prompt ("¿Qué producto quiere agregar al carrito?")
+                bandera = confirm ("¿Quiere realizar algun otro movimiento?")
+            break
+            default:
+               alert ("Error, lo que usted ingresó no es correcto")
+            break
+        }
+        if(bandera){
+            let opcion = Number(prompt("Bienvenido a LAZ, la mejor tienda onlione de productos electrónicos, a continuación indique la opción que corresponda: \n 1-Mostrar Productos Disponibles \n 2-Agregar Productos al Carrito"))
+        }
+        else{
+            alert ("Gracias por visitarnos")
+        }
+    }
+    
+}
+
+
+if (accesoatienda){
+    core()
+}
+else{
+    alert ("Gracias por visitarnos")
+}
